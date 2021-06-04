f4808e41f01e9c725e3390bb76017050ef8d3400
GuyAmir1122 <85358820+GuyAmir1122@users.noreply.github.com>
2021-06-04 18:39:52 +0300

    Add files via upload

---
+++ Apple.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Apple
+    {
+        
+
+        
+        public Apple()
+        {        
+        }
+        public void Draw(int xpos,int ypos) 
+        {
+            char ch = '█';
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.SetCursorPosition(xpos, ypos + i);
+                Console.Write(new string(ch, 5));
+            }
+            Console.SetCursorPosition(xpos + 2, ypos - 1);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(ch);
+        }
+        public void Erase(int xpos,int ypos)
+        {
+            char ch = '█';
+            Console.ForegroundColor = ConsoleColor.Black;
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.SetCursorPosition(xpos, ypos + i);
+                Console.Write(new string(ch, 5));
+            }
+            Console.SetCursorPosition(xpos + 2, ypos - 1);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(ch);
+        }
+        public void MoveDown(int xpos,int ypos ,int speed)
+        {
+            Draw(xpos,ypos);
+            for (int i = 1; i < 2; i++)
+            {
+                Erase(xpos,ypos);
+                ypos++;
+                Draw(xpos,ypos);
+                Thread.Sleep(speed);
+            }
+        }
+
+
+    }
+}
